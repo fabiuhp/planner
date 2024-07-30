d8aeb2213f988cd4e660362ad394b193803d350a
Fabio Pereira <fabiuhp@msn.com>
2024-07-29 21:28:35 -0300

    repo e entidade atividade

---
+++ src/main/java/br/com/fabiopereira/planner/activities/Activity.java
@@ -0,0 +1,26 @@
+package br.com.fabiopereira.planner.activities;
+
+import br.com.fabiopereira.planner.trip.Trip;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "activities")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Activity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    @Column(name = "occurs_at", nullable = false)
+    private LocalDateTime occursAt;
+    @ManyToOne
+    @JoinColumn(name = "trip_id", nullable = false)
+    private Trip trip;
+}
